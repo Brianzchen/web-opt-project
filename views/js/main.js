4c1dc5372960dfd82f7a2a93fa5916c7a5bb59bc
Brian Chen <bchen.chen93@gmail.com>
2015-07-01 21:22:00 +1200

    fixed the pizza.html

--- views/js/main.js
+++ views/js/main.js
@@ -475,8 +475,12 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+
+  // calculates a sin wave for the pizzas to move
+  var phase = Math.sin((document.body.scrollTop / 1250));
+
+  // Moves the pizzas in the background.
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
